bdcd8c1d9e4ac6144df6d1ef551adbc04d389ec4
Muhammad Abubakar <muhammadabubakar0419@gmail.com>
2018-04-30 21:30:36 -0400

    added some css style to default image and upload image button

--- client/src/components/ImageLinkForm.jsx
+++ client/src/components/ImageLinkForm.jsx
@@ -23,10 +23,7 @@ class ImageLinkForm extends Component {
           <span id="imageUpload" className="Qwigley">
             OR
           </span>
-          <a
-            className="btn-floating btn-large waves-effect waves-light "
-            onClick={this.props.toggleCameraState}
-          >
+          <a id="uploadImageBtn" className="btn-floating" onClick={this.props.toggleCameraState}>
             <i className="material-icons">add_a_photo</i>
           </a>
         </div>
