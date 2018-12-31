13e9f98929521c685241e87668c0a0cfab017c9a
Muhammad Abubakar <muhammadabubakar0419@gmail.com>
2018-12-31 06:37:58 -0500

    refactored the camera file and created separated file for Camera buttons

--- client/src/components/Camera.jsx
+++ client/src/components/Camera.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import takePhoto from "./camera/takePhoto";
 import stream from "./camera/stream";
+import Buttons from "./camera/buttons";
 
 class Camera extends Component {
   state = {
@@ -16,39 +17,17 @@ class Camera extends Component {
     this.props.onImageDrop([canvasUrl]);
   };
 
-  renderCameraBtns = () => {
-    return (
-      <ul id="cameraBtns">
-        <li>
-          <a
-            id="uploadImageBtn"
-            className="btn-floating btn-large"
-            onClick={this.callOnImageDrop}
-          >
-            <i className="material-icons">add_a_photo</i>
-          </a>
-        </li>
-        <li>
-          <a
-            id="uploadImageBtn"
-            className="btn-floating"
-            onClick={e => {
-              this.props.toggleCameraState();
-              stream.stop();
-            }}
-          >
-            <i className="material-icons">backspace</i>
-          </a>
-        </li>
-      </ul>
-    );
-  };
-
   render() {
     return (
       <React.Fragment>
         <video id={this.state.id} />
-        {this.renderCameraBtns()}
+        {
+          <Buttons
+            callOnImageDrop={this.callOnImageDrop}
+            toggleCameraState={this.props.toggleCameraState}
+            stream={stream}
+          />
+        }
       </React.Fragment>
     );
   }
